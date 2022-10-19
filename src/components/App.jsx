7312a6e63468fa1af71fca400579cb83f509307c
Anastasiia <101875515+Annix48@users.noreply.github.com>
2022-10-19 16:28:29 +0300

    First h/w

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+
+
+import { Profile } from './Profile/Profile';
+import user from '../config/user';
+
+import { Statistics } from './Statistics/Statistics';
+import data from '../config/data';
+
+// import { FriendList } from './FriendsList';
+// import friends from '../configs/friends';
+
+// import { TransactionHistory } from './TransactionHistory';
+// import transactions from '../configs/transactions';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    
+      <><Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats} />
+      <Statistics title="Upload stats" stats={data} /></> 
+      // <FriendList friends={friends} />
+      // <TransactionHistory items={transactions} /> */
+    
   );
 };
+
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+
+
+export const Statistics = ({ title,id, label, percentage }) => {
+  
+    return (
+        <section className = {css.statistics}>
+           {title && <h2 className={css.title}>{title}</h2>}
+
+            <ul className={css.list}>
+                <li className={css.item}>
+                    <span className = {css.label}>{label}</span>
+                    <span className = {css.percentage}>{percentage}</span>
+                </li>
+                <li className={css.item}>
+                    <span className = {css.label}>{label}</span>
+                    <span className = {css.percentage}>{percentage}</span>
+                </li>
+                <li className={css.item}>
+                    <span className = {css.label}>{label}</span>
+                    <span className = {css.percentage}>{percentage}</span>
+                </li>
+                <li className={css.item}>
+                    <span className = {css.label}>{label}</span>
+                    <span className = {css.percentage}>{percentage}</span>
+                </li>
+            </ul>
+        </section>
+
+    );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }).isRequired
+  ).isRequired,
+};
+
