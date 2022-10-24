eb49bfb4f68d8ffc2d699ab535211902d2e1e5fd
Anastasiia <101875515+Annix48@users.noreply.github.com>
2022-10-24 15:56:34 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,25 +6,27 @@ import user from '../config/user';
 import { Statistics } from './Statistics/Statistics';
 import data from '../config/data';
 
-// import { FriendList } from './FriendsList';
-// import friends from '../configs/friends';
+import { FriendList } from './FriendList';
+import friends from '../config/friends';
 
-// import { TransactionHistory } from './TransactionHistory';
-// import transactions from '../configs/transactions';
+import { TransactionHistory } from './TransactionHistory';
+import transactions from '../config/transactions';
 
 
 export const App = () => {
   return (
     
-      <><Profile
+    <><Profile
       username={user.username}
       tag={user.tag}
       location={user.location}
       avatar={user.avatar}
       stats={user.stats} />
-      <Statistics title="Upload stats" stats={data} /></> 
-      // <FriendList friends={friends} />
-      // <TransactionHistory items={transactions} /> */
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+     <TransactionHistory items={transactions} />
+    </>
+      
     
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,44 +1,37 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
+const colors = ['#984ED0', '#CDD25A', '#FDB05E', '#0475ED', '#F02C6E'];
 
-export const Statistics = ({ title,id, label, percentage }) => {
-  
+export const Statistics = ({ title, stats }) => {
+
     return (
-        <section className = {css.statistics}>
-           {title && <h2 className={css.title}>{title}</h2>}
+        <section className={css.statistics}>
+            {title && <h2 className={css.title}>{title}</h2>}
+
 
             <ul className={css.list}>
-                <li className={css.item}>
-                    <span className = {css.label}>{label}</span>
-                    <span className = {css.percentage}>{percentage}</span>
-                </li>
-                <li className={css.item}>
-                    <span className = {css.label}>{label}</span>
-                    <span className = {css.percentage}>{percentage}</span>
-                </li>
-                <li className={css.item}>
-                    <span className = {css.label}>{label}</span>
-                    <span className = {css.percentage}>{percentage}</span>
-                </li>
-                <li className={css.item}>
-                    <span className = {css.label}>{label}</span>
-                    <span className = {css.percentage}>{percentage}</span>
-                </li>
-            </ul>
+                 {stats.map(({ id, label, percentage }, index) => (
+                    <li className={css.item} key={id} style={{ background: colors[index] }}>
+                        <span className={css.label}>{label}</span>
+                        <span className={css.percentage}>{percentage}</span>
+                    </li>
+                ))}
+
+            </ul> 
         </section>
 
     );
 };
 
 Statistics.propTypes = {
-  title: PropTypes.string,
-  stats: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
-    }).isRequired
-  ).isRequired,
+    title: PropTypes.string.isRequired,
+    stats: PropTypes.arrayOf(
+        PropTypes.exact({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
+            percentage: PropTypes.number.isRequired,
+        }).isRequired
+    ).isRequired,
 };
 
